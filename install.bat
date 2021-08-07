f01588fbea8f7021627319c44dde5a65b4d4cb3d
Legrain Samuel <samuel.legrain@bstorm.be>
2021-08-07 21:37:29 +0200

    Add install.bat

---
+++ install.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Les fichiers manquants vont s'installer
+pause
+npm install
+cls
--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 @echo off
+if not exist .\node_modules\nul call install.bat
 echo SuperHeroAPI - Solution locale
 echo Le serveur sera accessible par le port 3000
 echo Appuyez CTRL + C pour stopper le serveur
